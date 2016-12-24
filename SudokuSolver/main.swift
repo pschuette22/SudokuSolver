c097c0ea0523176d80f5b9697ca006a7cd658ea7
i858331 <peter.schuette01@sap.com>
2016-12-24 15:35:55 -0600

    Initial Commit

---
+++ SudokuSolver/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SudokuSolver
+//
+//  Created by Schuette, Peter on 12/24/16.
+//  Copyright © 2016 Schuette, Peter. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
